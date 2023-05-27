dea4ef499a528a994ce3c4763f7d79054c45d66f
Bioturing <bioturing@MacBook-Pro-cua-Bioturing.local>
2023-05-27 09:39:50 +0700

    add

--- backend/auth/app/main.py
+++ backend/auth/app/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI, APIRouter
 from app.auth.model import PersonUpdate, Account
 from app.api import authentication, users
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
 import uvicorn
 
 router = APIRouter()
@@ -15,9 +17,13 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["get", "post", "options"],
     allow_headers=["*"],
-    max_age=10,
 )
+
+app.mount("/", StaticFiles(directory="/build", html=True))
+
+async def get_data():
+    # Your API logic here
+    return {"message": "Hello, world!"}
 
 app.include_router(authentication.router)
 app.include_router(users.router)
-
